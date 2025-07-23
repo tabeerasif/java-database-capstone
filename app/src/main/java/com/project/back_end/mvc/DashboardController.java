4762e183a6fb43fe57094f43252829e6ed8d7f31
Tabeer Asif <tabeer.asif@capgemini.com>
2025-07-23 23:34:39 +0100

    controller

--- app/src/main/java/com/project/back_end/mvc/DashboardController.java
+++ app/src/main/java/com/project/back_end/mvc/DashboardController.java
@@ -1,18 +1,49 @@
 package com.project.back_end.mvc;
 
+import java.security.Provider.Service;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class DashboardController {
 
-// 1. Set Up the MVC Controller Class:
-//    - Annotate the class with `@Controller` to indicate that it serves as an MVC controller returning view names (not JSON).
-//    - This class handles routing to admin and doctor dashboard pages based on token validation.
+    @Autowired
+    Service service;
 
+    @GetMapping("/adminDashboard/{token}")
+    public String adminDashboard(@PathVariable String token) {
+        Map<String, String> map = service.validateToken(token, "admin").getBody();
+        if(map.isEmpty())
+        {
+            return "admin/adminDashboard";
+        }
+        return "redirect:http://localhost:8080"; 
+    }
 
-// 2. Autowire the Shared Service:
-//    - Inject the common `Service` class, which provides the token validation logic used to authorize access to dashboards.
+    @GetMapping("/doctorDashboard/{token}")
+    public String doctorDashboard(@PathVariable String token)
+    {
+        Map<String, String> map=service.validateToken(token,"doctor").getBody();
+        System.out.println("map"+map);
+        if(map.isEmpty())
+        {
+            return "doctor/doctorDashboard";
+        }
+        
+        return "redirect:http://localhost:8080"; 
+        
+    }
+
+       
+
 
 
 // 3. Define the `adminDashboard` Method:
-//    - Handles HTTP GET requests to `/adminDashboard/{token}`.
+
 //    - Accepts an admin's token as a path variable.
 //    - Validates the token using the shared service for the `"admin"` role.
 //    - If the token is valid (i.e., no errors returned), forwards the user to the `"admin/adminDashboard"` view.
